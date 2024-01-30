f01cec01adeb01da144a8a271cdf16b6dce4bb41
kinto-b <kinto.behr@gmail.com>
2024-01-30 20:38:00 +1100

    Day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,13 +1,46 @@
 use crate::solution::Solution;
 
-pub fn part1() -> Solution {
-    let solution = 0;
+// TODO: Would be nice not to have to do the iter->vec->iter round trip.
+fn parse() -> Vec<[u8; 4]>{
+    include_str!("../../data/day04.txt")
+        .lines()
+        .map(parse_line)
+        .collect()
+}
 
-    Solution { solution }
+fn parse_line(x: &str) -> [u8; 4] {
+    x.split_terminator(['-', ','])
+        .map(|i| i.parse::<u8>().unwrap())
+        .collect::<Vec<u8>>()[..4]
+        .try_into()
+        .unwrap()
+}
+
+/// Returns true if either pair is fully contained by the other, else false
+fn full_overlap([l1, u1, l2, u2]: &[u8; 4]) -> bool {
+    ((l1 <= l2) && (u1 >= u2)) || // right in left
+    ((l2 <= l1) && (u2 >= u1))    // left in right
+}
+
+/// Returns true if either pair overlaps with the other, else false
+fn partial_overlap([l1, u1, l2, u2]: &[u8; 4]) -> bool {
+    ((l1 <= l2) & (u1 >= l2)) || // ( [ ) ]
+    ((l2 <= l1) & (u2 >= l1))    // [ ( ] )
+}
+
+/// Count the number of 'contained' pairs
+pub fn part1() -> Solution {
+    let solution = parse().iter()
+        .filter(|&v| full_overlap(v))
+        .count();
+
+    Solution { solution: solution as i32 }
 }
 
 pub fn part2() -> Solution {
-    let solution = 0;
+    let solution = parse().iter()
+        .filter(|&v| partial_overlap(v))
+        .count();
 
-    Solution { solution }
+    Solution { solution: solution as i32 }
 }

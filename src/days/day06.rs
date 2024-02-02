954a087ec3c88152a16aa7057ccf87a5fded8085
kinto-b <kinto.behr@gmail.com>
2024-02-03 08:18:35 +1100

    Improve day 6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,36 +1,28 @@
-//! Slide a window along the stream and check that the characters are unique 
-//! within the window.
-//! 
-//! TODO: This is somewhat wasteful since we effectively check each element
-//! as many times as the window is wide. It would be better to do something like
-//! 
-//! window = []
-//! if i := window.locate(x):
-//!     window = window[(i+1):] + [x]
-//! else:
-//!     window += [x]
-//! 
-//! and stop when window.len() == threshold.
 
-/// Returns true if all characters in the buffer are unique
-fn is_marker(buffer: &[u8]) -> bool {
-    let n = buffer.len() as u32;
-    n == buffer.iter()
-        .map(|i| 1 << (i - b'a' + 1))
-        .fold(0, |acc: u32, i: u32| acc | i )
-        .count_ones()
+/// Return the index of the first character which is preceeded by `size` unique
+/// characters (inclusive of final character).
+fn locate_marker(size: usize) -> usize {
+    let buffer = include_bytes!("../../data/day06.txt");
+    let mut window: Vec<u8> = Vec::new();
+
+    for (i, x) in buffer.iter().enumerate() {
+        if let Some(i) = window.iter().position(|w| w == x) {
+            window.drain(..(i+1));
+        }
+        window.push(*x);
+
+        if window.len() == size {
+            return i + 1;
+        }
+    }
+
+    unreachable!()
 }
 
 pub fn part1() -> usize {
-    4 + include_bytes!("../../data/day06.txt")
-        .windows(4)
-        .position(is_marker)
-        .unwrap()
+    locate_marker(4)
 }
 
 pub fn part2() -> usize {
-    14 + include_bytes!("../../data/day06.txt")
-        .windows(14)
-        .position(is_marker)
-        .unwrap()
+    locate_marker(14)
 }

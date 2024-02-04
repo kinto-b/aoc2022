69217a19a7acd5b09fa54aa826826a5a1d9a2253
kinto-b <kinto.behr@gmail.com>
2024-02-04 22:33:50 +1100

    Day 9

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,12 +1,69 @@
+//! Snaking ropes
 
-pub fn part1() -> i32 {
-    let solution = 0;
+use std::{collections::HashSet, fs::read_to_string};
 
-    solution
+/// Convert a step character into a vector (dx, dy)
+fn step(x: &str) -> (i32, i32) {
+    match x {
+        "U" => { (0, 1) },
+        "D" => { (0, -1) },
+        "L" => { (-1, 0) },
+        "R" => { (1, 0) },
+        _ => unreachable!()
+    }
 }
 
-pub fn part2() -> i32 {
-    let solution = 0;
+/// Returns a new tail coord after catching up with the head
+fn catchup((xh, yh): (i32, i32), (xt, yt): (i32, i32)) -> (i32, i32) {
+    let dx = xh - xt;
+    let dy = yh - yt;
 
-    solution
+    if dx.abs() > 1 || dy.abs() > 1 {
+        (xt + dx.signum(), yt + dy.signum())
+    } else {
+        (xt, yt)
+    }
+}
+
+/// Return the number of locations visited by the tail of a rope with a given 
+/// number of knots whose head follows a given set of directions.
+fn snake(knots: usize) -> usize {
+    let directions: String = read_to_string("data/day09.txt").unwrap();
+
+    let (mut x, mut y) = (0, 0);  // Head position
+    let mut rope = vec![(0, 0); knots-1]; // Remaining knots
+    
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    visited.insert((x, y));
+
+    for l in directions.lines() {
+        if let Some((d, s)) = l.split_once(' ') {
+            let (dx, dy) = step(d);
+            let nsteps = s.parse::<usize>().unwrap();
+            for _ in 0..nsteps {
+                x += dx;
+                y += dy;
+
+                rope = rope.iter().scan((x, y), |state, &(u, v)| {
+                    *state = catchup(*state, (u, v));
+                    Some(*state)
+                }).collect();
+                
+                visited.insert(*rope.last().unwrap());
+            }            
+        }
+    }
+
+    visited.len()
+}
+
+/// Returns the number of places visited by T in a two-knot rope
+pub fn part1() -> usize {
+    snake(2)
+}
+
+
+/// Returns the number of places visited by T in a ten-knot rope
+pub fn part2() -> usize {
+    snake(10)   
 }
